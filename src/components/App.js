d73cb878a2daa41886dc0a7ba0af84ca85b14efe
Dandy <dandyjreyes@gmail.com>
2020-05-16 20:16:32 -0700

    added google analytics to site for traffic

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React from "react";
-import Nav from "./Nav/Nav";
-import Name from "./Name/Name";
-import Title from "./Title/Title";
-import About from "./About/About";
-import Skills from "./Skills/Skills";
-import Projects from "./Projects/Projects";
-import MailForm from "./MailForm/MailForm";
+import React from 'react';
+import Nav from './Nav/Nav';
+import Name from './Name/Name';
+import Title from './Title/Title';
+import About from './About/About';
+import Skills from './Skills/Skills';
+import Projects from './Projects/Projects';
+import MailForm from './MailForm/MailForm';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-166869445-1');
+ReactGA.pageview('/');
 
 class App extends React.Component {
   render() {
