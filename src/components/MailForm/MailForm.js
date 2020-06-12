dbb9ae50f79b02f95d46fefa25903023bfc02929
Dandy <dandyjreyes@gmail.com>
2020-06-11 21:54:33 -0700

    added buffering animation for email sending

--- src/components/MailForm/MailForm.js
+++ src/components/MailForm/MailForm.js
@@ -9,6 +9,7 @@ class MailForm extends React.Component {
       name: '',
       email: '',
       bodyText: '',
+      isHidden: false,
     };
     this.initialState = this.state;
   }
@@ -25,26 +26,19 @@ class MailForm extends React.Component {
   handleSubmit = async (event) => {
     event.preventDefault();
     const { name, email, bodyText } = this.state;
-    let config = {
-      onUploadProgress: (progressEvent) => {
-        let percentCompleted = Math.floor(
-          (progressEvent.loaded * 100) / progressEvent.total
-        );
-        console.log(percentCompleted);
-        // do whatever you like with the percentage complete
-      },
-    };
+    if (!name || !email || !bodyText) {
+      alert('please fill out the whole form!');
+      return;
+    }
+    this.setState({ isHidden: true });
+    // Work on this functionality
     if (name && email && bodyText) {
       return await axios
-        .post(
-          'https://dandyportfolio.herokuapp.com/send',
-          {
-            name,
-            email,
-            bodyText,
-          },
-          config
-        )
+        .post('https://dandyportfolio.herokuapp.com/send', {
+          name,
+          email,
+          bodyText,
+        })
         .then(() => {
           this.setState(this.initialState);
           alert('message sent');
@@ -54,39 +48,47 @@ class MailForm extends React.Component {
           console.log(err.message);
         });
     }
-    alert('please fill out the whole form!');
   };
 
   render() {
-    const { name, email, bodyText } = this.state;
+    const { name, email, bodyText, isHidden } = this.state;
     return (
       <div id="contact">
         <form id="myData" onSubmit={this.handleSubmit} autoComplete="off">
           <h2 id="ending-title">
             Let's <b id="ending-title-outline">BUILD</b> Something!
           </h2>
-          <input
-            type="text"
-            name="name"
-            placeholder="Name"
-            value={name}
-            onChange={this.handleChange}
-          />
-          <input
-            type="email"
-            placeholder="Email"
-            name="email"
-            value={email}
-            onChange={this.handleChange}
-          />
-          <input
-            type="text"
-            placeholder="What can I help you with?"
-            name="bodyText"
-            value={bodyText || ''}
-            onChange={this.handleChange}
-          />
-          <button type="submit">Submit</button>
+          {!isHidden ? (
+            <>
+              <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                value={name}
+                onChange={this.handleChange}
+              />
+              <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                value={email}
+                onChange={this.handleChange}
+              />
+              <input
+                type="text"
+                placeholder="What can I help you with?"
+                name="bodyText"
+                value={bodyText || ''}
+                onChange={this.handleChange}
+              />
+
+              <button type="submit">Submit</button>
+            </>
+          ) : (
+            <div className="imgBuffer">
+              <img src="https://i.gifer.com/VAyR.gif" />
+            </div>
+          )}
         </form>
         <ul id="contact-info">
           <li>dandyjreyes@gmail.com</li>
