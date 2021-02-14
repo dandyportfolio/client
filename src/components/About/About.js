a34332c4fa17927dbc0424ed375e88e61d270028
Dandy <dandyjreyes@gmail.com>
2021-02-13 23:46:49 -0800

    Changed about component to function based component and updated gsap timeline

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../css/About.min.css';
-import { TimelineMax } from 'gsap';
+import gsap from 'gsap';
 import ScrollMagic from 'scrollmagic';
 import 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap';
 import '../../plugins/debug.addIndicators';
 
-class About extends React.Component {
-  componentDidMount() {
+export default function About() {
+  useEffect(() => {
     //! START ANIMATION
-    const tl = new TimelineMax({ onUpdate: updatePercentage });
+    const tl = gsap.timeline({ onUpdate: updatePercentage });
     const controller = new ScrollMagic.Controller();
 
     tl.from('.background-text', 2, { x: 700 });
@@ -24,38 +24,34 @@ class About extends React.Component {
     function updatePercentage() {
       tl.progress();
     }
-  }
+  });
 
-  render() {
-    return (
-      <div id="about" className="about-container">
-        <div>
-          <div className="my-portfolio-image-wrapper">
-            <img
-              className="my-portfolio-image"
-              src="/images/reyes_dandy_headshot.png"
-              alt="Portrait of myself"
-              width="100px"
-            />
-          </div>
-          <div className="about-me-wrapper">
-            <p>
-              Hola, my name is <i>Dandy</i>. I am a
-              <b> Latino Software Engineer</b> and <b>UI/UX Designer</b>. I love
-              coding, creating and helping people get to the next level in their
-              career path as developers. I advocate for foster youth and love to
-              mentor young developers.
-              <br />
-              <br />
-              Some of my crazy hobbies are building robots from the ground up,
-              biking, knitting, and throwing get togethers. I’m a people person!
-            </p>
-          </div>
-          <h1 className="background-text">ABOUT</h1>
+  return (
+    <div id="about" className="about-container">
+      <div>
+        <div className="my-portfolio-image-wrapper">
+          <img
+            className="my-portfolio-image"
+            src="/images/reyes_dandy_headshot.png"
+            alt="Portrait of myself"
+            width="100px"
+          />
         </div>
+        <div className="about-me-wrapper">
+          <p>
+            Hola, my name is <i>Dandy</i>. I am a
+            <b> Latino Software Engineer</b> and <b>UI/UX Designer</b>. I love
+            coding, creating and helping people get to the next level in their
+            career path as developers. I advocate for foster youth and love to
+            mentor young developers.
+            <br />
+            <br />
+            Some of my crazy hobbies are building robots from the ground up,
+            biking, knitting, and throwing get togethers. I’m a people person!
+          </p>
+        </div>
+        <h1 className="background-text">ABOUT</h1>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default About;
