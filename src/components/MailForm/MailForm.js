12b44d194a9ee496cbca859a8b9a7e0cc0170209
Dandy <dandyjreyes@gmail.com>
2021-02-14 11:18:35 -0800

    updated contact form from class to functional component

--- src/components/MailForm/MailForm.js
+++ src/components/MailForm/MailForm.js
@@ -1,37 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/MailForm.min.css';
 import axios from 'axios';
 
-class MailForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      email: '',
-      bodyText: '',
-      isHidden: false,
-    };
-    this.initialState = this.state;
-  }
+export default function MailForm() {
+  const [state, setState] = useState({
+    name: '',
+    email: '',
+    bodyText: '',
+    isHidden: false,
+  });
 
-  handleChange = (event) => {
-    const { target } = event;
-    const { value } = target;
-    const { name } = target;
-    this.setState({
-      [name]: value,
-    });
+  const handleChange = event => {
+    const { value, name } = event.target;
+
+    setState(prevState => ({ ...prevState, [name]: value }));
   };
 
-  handleSubmit = async (event) => {
+  const handleSubmit = async event => {
     event.preventDefault();
-    const { name, email, bodyText } = this.state;
+    const { name, email, bodyText } = state;
     if (!name || !email || !bodyText) {
       alert('please fill out the whole form!');
       return;
     }
-    this.setState({ isHidden: true });
-    // Work on this functionality
+    setState({ isHidden: true });
     if (name && email && bodyText) {
       return await axios
         .post('https://dandyportfolio.herokuapp.com/send', {
@@ -40,68 +32,64 @@ class MailForm extends React.Component {
           bodyText,
         })
         .then(() => {
-          this.setState(this.initialState);
           alert('message sent');
+          setState(prevState => ({ ...prevState, isHidden: false }));
         })
-        .catch((err) => {
+        .catch(err => {
           alert('Message was not able to send');
           console.log(err.message);
         });
     }
   };
 
-  wordCount = (val) => {
+  const wordCount = val => {
     return val ? `word count: ${val.length}` : `word count: 0`;
   };
 
-  render() {
-    const { name, email, bodyText, isHidden } = this.state;
-    return (
-      <div id="contact">
-        <form id="myData" onSubmit={this.handleSubmit} autoComplete="off">
-          <h2 id="ending-title">
-            Let's <b id="ending-title-outline">BUILD</b> Something!
-          </h2>
-          {!isHidden ? (
-            <>
-              <input
-                type="text"
-                name="name"
-                placeholder="Name"
-                value={name}
-                onChange={this.handleChange}
-              />
-              <input
-                type="email"
-                placeholder="Email"
-                name="email"
-                value={email}
-                onChange={this.handleChange}
-              />
-              <input
-                type="text"
-                placeholder="What can I help you with?"
-                name="bodyText"
-                value={bodyText || ''}
-                onChange={this.handleChange}
-              />
-              <div id="countWord">{this.wordCount(bodyText)}</div>
+  const { name, email, bodyText, isHidden } = state;
+  return (
+    <div id="contact">
+      <form id="myData" onSubmit={handleSubmit} autoComplete="off">
+        <h2 id="ending-title">
+          Let's <b id="ending-title-outline">BUILD</b> Something!
+        </h2>
+        {!isHidden ? (
+          <>
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={name}
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              value={email}
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder="What can I help you with?"
+              name="bodyText"
+              value={bodyText || ''}
+              onChange={handleChange}
+            />
+            <div id="countWord">{wordCount(bodyText)}</div>
 
-              <button type="submit">Submit</button>
-            </>
-          ) : (
-            <div className="imgBuffer">
-              <img src="https://i.gifer.com/VAyR.gif" />
-            </div>
-          )}
-        </form>
-        <ul id="contact-info">
-          <li>dandyjreyes@gmail.com</li>
-          <li>+1 (323) 251-8132</li>
-        </ul>
-      </div>
-    );
-  }
+            <button type="submit">Submit</button>
+          </>
+        ) : (
+          <div className="imgBuffer">
+            <img src="https://i.gifer.com/VAyR.gif" />
+          </div>
+        )}
+      </form>
+      <ul id="contact-info">
+        <li>dandyjreyes@gmail.com</li>
+        <li>+1 (323) 251-8132</li>
+      </ul>
+    </div>
+  );
 }
-
-export default MailForm;
